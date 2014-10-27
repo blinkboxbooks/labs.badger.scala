891da1b3d219d2586377b5ab54cc2b12339d6338
Alin Popa <alin.popa@gmail.com>
2014-10-27 11:41:25 +0000

    A bit of code cleanup
    - use localhost env for cassandra, postgres and rabbitmq

--- app/conf/Global.scala
+++ app/conf/Global.scala
@@ -15,25 +15,42 @@ object Global extends GlobalSettings{
   private var _eventsDb: EventsDB = _
 
   override def onStart(app: Application): Unit = {
-    val configuration = current.configuration
-    _badgesDb = new JdbcBadgesDB
-    Logger.info("DB connection started.")
-    _eventsDb = new CassandraEventsDB(configuration.underlying)
-    Logger.info("EventsDB connection started.")
-    _users = new DbUserService(badgesDb)
-    val messagingEc = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(3))
-    val messaging = Future{
-      new MessagingContext(Akka.system, configuration.underlying)
-      Logger.info("Messaging connection started.")
-    }(messagingEc)
-    messaging.onFailure{
-      case e => Logger.error("Something happened with messaging", e)
-    }(messagingEc)
+    initBadgesDb()
+    initEventsDb()
+    initUsersService()
+    initMessaging()
     Logger.info("Badger started.")
   }
 
   override def onStop(app: Application): Unit = {
     Logger.info("Badger stopped.")
+  }
+
+  private def initBadgesDb(): Unit = {
+    _badgesDb = new JdbcBadgesDB
+    Logger.info("DB connection started.")
+  }
+
+  private def initEventsDb(): Unit = {
+    implicit val configuration = current.configuration
+    _eventsDb = new CassandraEventsDB(configuration.underlying)
+    Logger.info("EventsDB connection started.")
+  }
+
+  private def initUsersService(): Unit = {
+    _users = new DbUserService(badgesDb)
+  }
+
+  private def initMessaging(): Unit = {
+    implicit val configuration = current.configuration
+    implicit val msgEc = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(3))
+    val messaging = Future{
+      new MessagingContext(Akka.system, configuration.underlying)
+      Logger.info("Messaging connection started.")
+    }
+    messaging.onFailure{
+      case e => Logger.error("Something happened with messaging", e)
+    }
   }
 
   def badgesDb = _badgesDb
